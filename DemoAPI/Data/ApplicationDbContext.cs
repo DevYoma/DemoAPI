269a57d5c9c88fe83335595bce1fde85fbe8b889
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-03-29 01:39:29 +0100

    Add project files.

---
+++ DemoAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<StudentEntity> StudentRegister { get; set; } // Students will be the name on the table
+    }
+}
